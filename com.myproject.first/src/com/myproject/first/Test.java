573c2ae88f65f567648cc487db17fc251da03b10
kossi.jorjinho <kossi.gbegnon@utbm.fr>
2021-04-24 23:56:05 +0200

    Test commit

--- com.myproject.first/src/com/myproject/first/Test.java
+++ com.myproject.first/src/com/myproject/first/Test.java
@@ -3,6 +3,6 @@ package com.myproject.first;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Git is fun");
+		System.out.println("Git is fun!");
 	}
 }
