51cf7bf71afa8f183b6917bef305cfae5245599e
kossi.jorjinho <kossi.gbegnon@utbm.fr>
2021-04-26 13:56:36 +0200

    1.2.3 Creation of the tablea account
    Modify Main class
    
     1 - Declares array of accounts
     2 - Adds method loadAccountArray
     3 - Adds method displayCountArray
     4 - Calls method in main method

--- KGBank/src/com/hnformation/excercice/principale/MainTest.java
+++ KGBank/src/com/hnformation/excercice/principale/MainTest.java
@@ -6,18 +6,20 @@ package com.hnformation.excercice.principale;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import com.hnformation.excercice.excercice1.Account;
 import com.hnformation.excercice.excercice1.Client;
+import com.hnformation.excercice.excercice1.CurrentAccount;
+import com.hnformation.excercice.excercice1.SavingsAccount;
 
 /**
  * @author jorji
  *
  */
 public class MainTest {
-	static private Client[] clientArray = { new Client() };
+	private static Client[] clientArray;
+	private static Account[][] accountArray;
 
 	/**
-	 * Excercice 1.1.2 creation of main class for tests
-	 *
 	 * Main method
 	 *
 	 * @param args
@@ -25,6 +27,10 @@ public class MainTest {
 	public static void main(String[] args) {
 		clientArray = loadClientArray(5);
 		displayClientArray(clientArray);
+
+		accountArray = loadAccountArray(clientArray);
+		displayAccountArray(accountArray);
+
 	}
 
 	/**
@@ -34,7 +40,7 @@ public class MainTest {
 	 * @return clientArray filled
 	 */
 	private static Client[] loadClientArray(int arraySize) {
-		clientArray = new Client[arraySize];
+		Client[] clientArray = new Client[arraySize];
 		final String NAME = "name";
 		final String FIRSTNAME = "firstName";
 		for (int i = 0; i < arraySize; i++) {
@@ -45,10 +51,45 @@ public class MainTest {
 
 	/**
 	 * This method displays the clientArray
+	 *
+	 * @param clientArray
 	 */
 	private static void displayClientArray(Client[] clientArray) {
 		Stream<Client> clientStream = Arrays.stream(clientArray);
 		clientStream.forEach(client -> System.out.println(client.toString()));
+	}
+
+	/**
+	 * this methods load accounts for all client
+	 *
+	 * @param clientArray
+	 * @return
+	 */
+	private static Account[][] loadAccountArray(Client[] clientArray) {
+		int counter = 0;
+		final String SAVINGS_ACCOUNT_LABEL = "Savings_account";
+		final String CURRENT_ACCOUNT_LABEL = "Current_account";
+		Account[][] accountArray = new Account[2][clientArray.length];
+		for (Client client : clientArray) {
+			Account currentAccount = new CurrentAccount(CURRENT_ACCOUNT_LABEL, client);
+			Account savingsAccount = new SavingsAccount(SAVINGS_ACCOUNT_LABEL, client);
+			accountArray[0][counter] = currentAccount;
+			accountArray[1][counter] = savingsAccount;
+			counter++;
+		}
+		return accountArray;
+	}
+
+	/**
+	 * This method displays the accountArray
+	 *
+	 * @param accountArray
+	 */
+	private static void displayAccountArray(Account[][] accountArray) {
+		Stream<Account[]> accountStream = Arrays.stream(accountArray);
+		accountStream.forEach(value -> {
+			Arrays.stream(value).forEach(account -> System.out.println(account.toString()));
+		});
 	}
 
 }
