f36f04299eedc588f2bd24b3e8ce8b33914302af
kossi.jorjinho <kossi.gbegnon@utbm.fr>
2021-04-26 13:52:04 +0200

    Adds constructors

--- KGBank/src/com/hnformation/excercice/excercice1/CurrentAccount.java
+++ KGBank/src/com/hnformation/excercice/excercice1/CurrentAccount.java
@@ -9,4 +9,10 @@ package com.hnformation.excercice.excercice1;
  */
 public class CurrentAccount extends Account {
 	/* 1.2.2 Creation of the CurrentAccount and SavingsAccount */
+	public CurrentAccount() {
+	}
+
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+	}
 }
