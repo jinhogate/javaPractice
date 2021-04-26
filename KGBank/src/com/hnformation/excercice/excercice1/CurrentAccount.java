04d7e24e7bdb723daf67500daa8fd8409568bb3b
kossi.jorjinho <kossi.gbegnon@utbm.fr>
2021-04-26 10:44:09 +0200

    1.2.2 Creation of the CurrentAccount
      1 - Creates CurrentAccount child class of Account

---
+++ KGBank/src/com/hnformation/excercice/excercice1/CurrentAccount.java
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+package com.hnformation.excercice.excercice1;
+
+/**
+ * @author jorji
+ *
+ */
+public class CurrentAccount extends Account {
+	/* 1.2.2 Creation of the CurrentAccount and SavingsAccount */
+}
