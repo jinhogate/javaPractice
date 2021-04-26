975364cfe43e2839f6bf6b24242167dc470945f3
kossi.jorjinho <kossi.gbegnon@utbm.fr>
2021-04-26 04:21:41 +0200

    add first project # ignored

--- com.myproject.first/src/com/myproject/first/GitTest.java
+++ com.myproject.first/src/com/myproject/first/GitTest.java
@@ -1,5 +1,26 @@
 package com.myproject.first;
 
-public class GitTest {
+import java.util.List;
+import java.util.stream.Stream;
 
+public class GitTest {
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] tab = { 12, 14, 58 };
+		Operation op = new Operateur();
+		op.afficher();
+		int res = op.calculer(15, 17);
+		System.out.println("Calcul = " + res);
+		String message = Operation.trouver(tab, 14) == -1 ? "pas trouvé!" : "trouvé!";
+		System.out.println("Message : " + message);
+		List<Integer> numbers = List.of(1, 24, 50);
+		Stream<Integer> streamNumbers = numbers.stream();
+		streamNumbers.forEach(valeur -> System.out.println("Valeur liste = " + valeur));
+		op.afficherAgain();
+	}
 }
--- com.myproject.first/src/com/myproject/first/Test.java
+++ com.myproject.first/src/com/myproject/first/Test.java
@@ -1,8 +1,20 @@
 package com.myproject.first;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws IOException {
+		String message = "";
+		System.out.println("message = " + message.isEmpty());
 		System.out.println("Git is fun!");
+		String str = "JD\nJD\nJD";
+		System.out.println(str.lines().collect(Collectors.toList()));
+		Path path = Files.writeString(Files.createTempFile("test", ".txt"), "I'am writing in this file");
+		System.out.println(path);
+		String s = Files.readString(path);
+		System.out.println(s); // This was posted on JD
 	}
 }
